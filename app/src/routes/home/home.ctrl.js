4a99a2b9fc1359704090a9dbdcb6d2f1db35c071
jinhobyeong <jhb_0921@naver.com>
2022-11-29 20:36:16 +0900

    login 프론트 기능 추가

--- app/src/routes/home/home.ctrl.js
+++ app/src/routes/home/home.ctrl.js
@@ -1,14 +1,42 @@
 "use strict";
 
-const home = (req, res) => {
-  res.render("home/index");
+const output = {
+  home: (req, res) => {
+    res.render("home/index");
+  },
+
+  login: (req, res) => {
+    res.render("home/login");
+  },
 };
 
-const login = (req, res) => {
-  res.render("home/login");
+const users = {
+  id: ["bicco2", "응애", "응애2"],
+  password: ["1234", "1234", "123456"],
+};
+
+const process = {
+  login: (req, res) => {
+    const id = req.body.id,
+      password = req.body.password;
+
+    if (users.id.includes(id)) {
+      const idx = users.id.indexOf(id);
+      if (users.password[idx] === password) {
+        return res.json({
+          success: true,
+        });
+      }
+    }
+
+    return res.json({
+      success: false,
+      msg: "로그인에 실패하셨습니다.",
+    });
+  },
 };
 
 module.exports = {
-  home,
-  login,
+  output,
+  process,
 };
