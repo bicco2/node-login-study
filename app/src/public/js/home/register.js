878aa3e6641e8276156fc78c0acc5ac1ad7aad91
jinhobyeong <jhb_0921@naver.com>
2022-11-30 19:52:38 +0900

    회원가입 로직 구현

---
+++ app/src/public/js/home/register.js
@@ -0,0 +1,43 @@
+"use strict";
+
+const id = document.querySelector("#id"),
+  name = document.querySelector("#name"),
+  password = document.querySelector("#password"),
+  confirmPassword = document.querySelector("#confirm-password"),
+  registerBtn = document.querySelector("#button");
+console.log("hello register");
+
+registerBtn.addEventListener("click", register);
+
+function register() {
+  if (!id.value) return alert("아이디를 입력하세요.");
+  if (password !== confirmPassword)
+    return alert("비밀번호가 일치하지않습니다.");
+
+  const req = {
+    id: id.value,
+    name: name.value,
+    password: password.value,
+    confirmPassword: confirmPassword.value,
+  };
+  console.log(req);
+
+  fetch("/register", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(req),
+  })
+    .then((res) => res.json())
+    .then((res) => {
+      if (res.success) {
+        location.href = "/login";
+      } else {
+        alert(res.msg);
+      }
+    })
+    .catch((err) => {
+      console.error("회원가입 중 에러 발생");
+    });
+}
